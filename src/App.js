9527bdf962fea0611f42821926a0aa57bdeaecca
ryo <ryo.kawahara.1992@gmail.com>
2025-07-31 22:20:53 +0900

    編集

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const API_ENDPOINT1 = 'https://vrt5rw6a10.execute-api.ap-northeast-1.amazonaws.c
 const API_ENDPOINT2 = 'https://vrt5rw6a10.execute-api.ap-northeast-1.amazonaws.com/ms_message_2';
 
 function App() {
-  const [result, setResult] = useState('結果がここに表示されます');
+  const [result, setResult] = useState('ここを編集しました');
   const [loadingButton, setLoadingButton] = useState('');
 
   const callLambda = async (endpoint, buttonId) => {
